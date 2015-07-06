c071416a2db204f74ab938a22d0c5e7d437d66ff
Callum2409 <pryercj@outlook.com>
2015-07-06 21:07:28 +0100

    Attempted to add in code for fission

--- GetMat.m
+++ GetMat.m
@@ -1,4 +1,4 @@
-function [t, a, s] = GetMat(material)
+function [t, a, s, f] = GetMat(material)
 %GETMAT
 %calculate and return the information for the required material
 
@@ -13,41 +13,47 @@ switch(lower(material))
 %mass, density, absorbtion cross section, scattering cross section
 %uses cgs units
 
-%need to add in fission x sect
     case {'water', 'h2o'}
-        [t, a, s] = MaterialInfo(18, 1, 0.6652, 103.0);
+        [t, a, s, f] = MaterialInfo(18, 1, 0.6652, 103.0);
     case {'lead', 'pb'}
-        [t, a, s] = MaterialInfo(207.2, 11.34, 0.158, 11.221);
+        [t, a, s, f] = MaterialInfo(207.2, 11.34, 0.158, 11.221);
     case 'graphite'
-        [t, a, s] = MaterialInfo(12, 1.67, 0.0045, 4.74);
+        [t, a, s, f] = MaterialInfo(12, 1.67, 0.0045, 4.74);
     case {'aluminium', 'al'}
-        [t, a, s] = MaterialInfo(26.982, 2.7, 0.189, 1.45);
+        [t, a, s, f] = MaterialInfo(26.982, 2.7, 0.189, 1.45);
     case {'silicon', 'si'}
-        [t, a, s] = MaterialInfo(28.086, 2.33, 0.152, 2.101);
+        [t, a, s, f] = MaterialInfo(28.086, 2.33, 0.152, 2.101);
     case {'iron', 'fe'}
-        [t, a, s] = MaterialInfo(55.845, 7.874, 2.292, 11.330);
+        [t, a, s, f] = MaterialInfo(55.845, 7.874, 2.292, 11.330);
     case {'lithium', 'li'}
-        [t, a, s] = MaterialInfo(6.94, 0.534, 62.53, 1.097);
+        [t, a, s, f] = MaterialInfo(6.94, 0.534, 62.53, 1.097);
     case {'cadmium', 'cd'}
-        [t, a, s] = MaterialInfo(112.414, 8.65, 2918.3, 10.237);
+        [t, a, s, f] = MaterialInfo(112.414, 8.65, 2918.3, 10.237);
     case {'heavy water', 'd2o'}
-        [t, a, s] = MaterialInfo(20.0276, 1.107, 5.2e-4, 19.53);
+        [t, a, s, f] = MaterialInfo(20.0276, 1.107, 5.2e-4, 19.53);
     case {'air', 'invis'}
-        [t, a, s] = deal(0, 0, 0);
-    case 'test'
-        [t, a, s] = MaterialInfo(112.6, 6.17, .4116, 57.1105);
+        [t, a, s, f] = deal(0, 0, 0, 0);
+    case {'uranium233', 'u233'}
+        [t, a, s, f] = MaterialInfo(238.02891, 19.05, 42.20, 12.19, 468.20);
 end
 end
 
-function [sigT, sigA, sigS] = MaterialInfo(mass, density, sigmaA, sigmaS)
+function [sigT, sigA, sigS, sigF] = MaterialInfo(mass, density, sigmaC, sigmaS, sigmaF)
 %MATERIALINFO
 %calculate the values given the cross sections
 Na = 6.0221413e23;%per mol
 barn = 1e-24;%in cm^2
 
 n = density * Na/mass;
-sigA = n*sigmaA*barn;
+
+if nargin == 4 %if not fissile
+    sigmaF = 0;%set fissile to be 0
+    sigF = 0;
+else
+    sigF = n*sigmaF*barn;%or calculate fission cross section
+end
+
+sigA = n*barn*(sigmaC+sigmaF);
 sigS = n*sigmaS*barn;
 sigT = sigA+sigS;
 end
-
--- Voxel.m
+++ Voxel.m
@@ -1,10 +1,14 @@
 close all;
 clear all;
 
-materials = {'water', 'lead', 'air', 'invis', 'si', 'fe', 'graphite'};%the material list
+materials = {'water', 'lead', 'air', 'invis', 'si', 'fe', 'graphite', 'U233'};%the material list
 voxelSize = .5;%size of each voxel
-neutrons = 100;%number of neutrons
+neutrons = 1;%number of neutrons
 show = 1; %set to 0 to not produce graphs
+
+%DO NOT CHANGE THIS
+[rx, ry, rz] = deal(0, 0, 0);%used for rotation initialisation
+initRot = 0;%if specific rotation or isotropic
 
 %the numbers in each group between semicolons are increasing in x
 %x1, x2; x1, x2....
@@ -16,7 +20,7 @@ show = 1; %set to 0 to not produce graphs
 %the point is relative to the model and will be scaled with the voxel size
 
 %rotation can be made to be directional, using deal(_, _, _) or isotropic
-%by using OnUnitSphere.
+%by setting initRot to be 1
 
 
 %define the voxel materials. numbers reference the above materials array
@@ -29,7 +33,7 @@ voxMat(:, :, 3) = [1, 1, 1, 1, 1; 1, 1, 1, 1, 1];
 voxMat(:, :, 4) = [1, 1, 1, 1, 1; 1, 1, 1, 1, 1];
 voxMat(:, :, 5) = [1, 1, 1, 1, 1; 1, 1, 1, 1, 1];
 point = [0, 2.5, 2.5];
-rotation = 'deal(1, 0, 0)';
+[x, y, z] = deal(1, 0, 0);
 %}
 
 %hole of water through lead
@@ -38,7 +42,7 @@ voxMat          = [1, 1, 1; 1, 1, 1; 2, 2, 2; 2, 2, 2; 2, 2, 2; 2, 2, 2];
 voxMat(:, :, 2) = [1, 1, 1; 1, 1, 1; 2, 1, 2; 2, 1, 2; 2, 1, 2; 2, 1, 2];
 voxMat(:, :, 3) = [1, 1, 1; 1, 1, 1; 2, 2, 2; 2, 2, 2; 2, 2, 2; 2, 2, 2];
 point = [0, 1.5, 1.5];
-rotation = 'deal(1, 0, 0)';
+[x, y, z] = deal(1, 0, 0);
 %}
 
 %water beginning end, vacuum hole in lead
@@ -47,7 +51,7 @@ voxMat          = [1, 1, 1; 1, 1, 1; 2, 2, 2; 2, 2, 2; 2, 2, 2; 2, 2, 2; 1, 1, 1
 voxMat(:, :, 2) = [1, 1, 1; 1, 1, 1; 2, 3, 2; 2, 3, 2; 2, 3, 2; 2, 3, 2; 1, 1, 1; 1, 1, 1];
 voxMat(:, :, 3) = [1, 1, 1; 1, 1, 1; 2, 2, 2; 2, 2, 2; 2, 2, 2; 2, 2, 2; 1, 1, 1; 1, 1, 1];
 point = [0, 1.5, 1.5];
-rotation = 'deal(1, 0, 0)';
+[x, y, z] = deal(1, 0, 0);
 %}
 
 %lead cube with air gap
@@ -58,7 +62,7 @@ voxMat(:, :, 3) = [2, 2, 3, 2, 2; 2, 2, 3, 2, 2; 3, 3, 3, 3, 3; 2, 2, 3, 2, 2; 2
 voxMat(:, :, 4) = [2, 2, 2, 2, 2; 2, 2, 2, 2, 2; 2, 2, 3, 2, 2; 2, 2, 2, 2, 2; 2, 2, 2, 2, 2];
 voxMat(:, :, 5) = [2, 2, 2, 2, 2; 2, 2, 2, 2, 2; 2, 2, 3, 2, 2; 2, 2, 2, 2, 2; 2, 2, 2, 2, 2];
 point = [2.5, 2.5, 2.5];
-rotation = 'OnUnitSphere';
+initRot = 1;
 %}
 
 %water cube with air gap
@@ -69,7 +73,7 @@ voxMat(:, :, 3) = [1, 1, 3, 1, 1; 1, 1, 3, 1, 1; 3, 3, 3, 3, 3; 1, 1, 3, 1, 1; 1
 voxMat(:, :, 4) = [1, 1, 1, 1, 1; 1, 1, 1, 1, 1; 1, 1, 3, 1, 1; 1, 1, 1, 1, 1; 1, 1, 1, 1, 1];
 voxMat(:, :, 5) = [1, 1, 1, 1, 1; 1, 1, 1, 1, 1; 1, 1, 3, 1, 1; 1, 1, 1, 1, 1; 1, 1, 1, 1, 1];
 point = [2.5, 2.5, 2.5];
-rotation = 'OnUnitSphere';
+initRot = 1;
 %}
 
 %water cube with air gap 2
@@ -80,7 +84,7 @@ voxMat(:, :, 3) = [4, 1, 3, 1, 4; 1, 1, 3, 1, 1; 3, 3, 3, 3, 3; 1, 1, 3, 1, 1; 4
 voxMat(:, :, 4) = [4, 1, 1, 1, 4; 1, 1, 1, 1, 1; 1, 1, 3, 1, 1; 1, 1, 1, 1, 1; 4, 1, 1, 1, 4];
 voxMat(:, :, 5) = [4, 4, 4, 4, 4; 4, 1, 1, 1, 4; 4, 1, 3, 1, 4; 4, 1, 1, 1, 4; 4, 4, 4, 4, 4];
 point = [2.5, 2.5, 2.5];
-rotation = 'OnUnitSphere';
+initRot = 1;
 %}
 
 %cube with air gap and water outside
@@ -95,7 +99,7 @@ voxMat(:, :, 7) = [4, 4, 4, 4, 4, 4, 4, 4, 4; 4, 4, 4, 4, 4, 4, 4, 4, 4; 4, 4, 2
 voxMat(:, :, 8) = [4, 4, 4, 4, 4, 4, 4, 4, 4; 4, 4, 4, 4, 4, 4, 4, 4, 4; 4, 4, 4, 4, 4, 4, 4, 4, 4; 4, 4, 4, 1, 1, 1, 4, 4, 4; 4, 4, 4, 1, 1, 1, 4, 4, 4; 4, 4, 4, 1, 1, 1, 4, 4, 4; 4, 4, 4, 4, 4, 4, 4, 4, 4; 4, 4, 4, 4, 4, 4, 4, 4, 4; 4, 4, 4, 4, 4, 4, 4, 4, 4];
 voxMat(:, :, 9) = [4, 4, 4, 4, 4, 4, 4, 4, 4; 4, 4, 4, 4, 4, 4, 4, 4, 4; 4, 4, 4, 4, 4, 4, 4, 4, 4; 4, 4, 4, 1, 1, 1, 4, 4, 4; 4, 4, 4, 1, 1, 1, 4, 4, 4; 4, 4, 4, 1, 1, 1, 4, 4, 4; 4, 4, 4, 4, 4, 4, 4, 4, 4; 4, 4, 4, 4, 4, 4, 4, 4, 4; 4, 4, 4, 4, 4, 4, 4, 4, 4];
 point = [4.5, 4.5, 4.5];
-rotation = 'OnUnitSphere';
+initRot = 1;
 %}
 
 %neutron tank
@@ -110,7 +114,7 @@ voxMat(:, :, 7) = [7, 7, 7, 7, 7, 7, 7, 7, 7; 7, 1, 1, 1, 1, 1, 1, 1, 7; 7, 1, 1
 voxMat(:, :, 8) = [7, 7, 7, 7, 7, 7, 7, 7, 7; 7, 1, 1, 1, 1, 1, 1, 1, 7; 7, 1, 1, 1, 1, 1, 1, 1, 7; 7, 1, 1, 1, 1, 1, 1, 1, 7; 7, 1, 1, 1, 1, 1, 1, 1, 7; 7, 1, 1, 1, 1, 1, 1, 1, 7; 7, 1, 1, 1, 1, 1, 1, 1, 7; 7, 1, 1, 1, 1, 1, 1, 1, 7; 7, 7, 7, 7, 7, 7, 7, 7, 7];
 voxMat(:, :, 9) = [7, 7, 7, 7, 7, 7, 7, 7, 7; 7, 7, 7, 7, 7, 7, 7, 7, 7; 7, 7, 7, 7, 7, 7, 7, 7, 7; 7, 7, 7, 7, 7, 7, 7, 7, 7; 7, 7, 7, 7, 7, 7, 7, 7, 7; 7, 7, 7, 7, 7, 7, 7, 7, 7; 7, 7, 7, 7, 7, 7, 7, 7, 7; 7, 7, 7, 7, 7, 7, 7, 7, 7; 7, 7, 7, 7, 7, 7, 7, 7, 7];
 point = [4.5, 4.5, 4.5];
-rotation = 'OnUnitSphere';
+initRot = 1;
 %}
 
 %lead block
@@ -127,8 +131,8 @@ voxMat(:, :, 9) = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2; 2, 2, 2, 2, 2, 2, 2, 2, 2, 2; 2
 voxMat(:, :,10) = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2; 2, 2, 2, 2, 2, 2, 2, 2, 2, 2; 2, 2, 2, 2, 2, 2, 2, 2, 2, 2; 2, 2, 2, 2, 2, 2, 2, 2, 2, 2; 2, 2, 2, 2, 2, 2, 2, 2, 2, 2; 2, 2, 2, 2, 2, 2, 2, 2, 2, 2; 2, 2, 2, 2, 2, 2, 2, 2, 2, 2; 2, 2, 2, 2, 2, 2, 2, 2, 2, 2; 2, 2, 2, 2, 2, 2, 2, 2, 2, 2; 2, 2, 2, 2, 2, 2, 2, 2, 2, 2];
 point = [5, 5, 5];
 %point = [0, 4.5, 4.5];
-%rotation = 'deal(1, 0, 0)';
-rotation = 'OnUnitSphere';
+%[x, y, z] = deal(1, 0, 0);
+initRot = 1;
 %}
 
 %lead/water block, stripes
@@ -145,8 +149,8 @@ voxMat(:, :, 9) = [1, 2, 1, 2, 1, 2, 1, 2, 1, 2; 1, 2, 1, 2, 1, 2, 1, 2, 1, 2; 1
 voxMat(:, :,10) = [1, 2, 1, 2, 1, 2, 1, 2, 1, 2; 1, 2, 1, 2, 1, 2, 1, 2, 1, 2; 1, 2, 1, 2, 1, 2, 1, 2, 1, 2; 1, 2, 1, 2, 1, 2, 1, 2, 1, 2; 1, 2, 1, 2, 1, 2, 1, 2, 1, 2; 1, 2, 1, 2, 1, 2, 1, 2, 1, 2; 1, 2, 1, 2, 1, 2, 1, 2, 1, 2; 1, 2, 1, 2, 1, 2, 1, 2, 1, 2; 1, 2, 1, 2, 1, 2, 1, 2, 1, 2; 1, 2, 1, 2, 1, 2, 1, 2, 1, 2];
 %point = [4.5, 4.5, 4.5];
 point = [0, 4.5, 4.5];
-rotation = 'deal(1, 0, 0)';
-%rotation = 'OnUnitSphere';
+[x, y, z] = deal(1, 0, 0);
+%initRot = 1;
 %}
 
 %lead/water block, alternating
@@ -164,17 +168,17 @@ voxMat(:, :,10) = [2, 1, 2, 1, 2, 1, 2, 1, 2, 1; 1, 2, 1, 2, 1, 2, 1, 2, 1, 2; 2
 %point = [4.5, 4.5, 4.5];
 point = [0, 5, 5];
 %point = [0, 4.5, 4.5];
-rotation = 'deal(1, 0, 0)';
-%rotation = 'OnUnitSphere';
+[x, y, z] = deal(1, 0, 0);
+%initRot = 1;
 %}
 
 %lead/water alternating
-%
+%{
 voxMat          = [1, 2; 2, 1];
 voxMat(:, :, 2) = [2, 1; 1, 2];
 voxMat = repmat(voxMat, [2, 5, 5]);
 point = [0, 5, 5];
-rotation = 'deal(1, 0, 0)';
+[x, y, z] = deal(1, 0, 0);
 %}
 
 %neutron guide: a graphite column inside water
@@ -185,7 +189,7 @@ voxMat(:, :, 3) = [1, 1, 7, 1, 1; 1, 1, 7, 1, 1; 1, 1, 7, 1, 1; 1, 1, 7, 1, 1; 1
 voxMat(:, :, 4) = [1, 1, 1, 1, 1; 1, 1, 1, 1, 1; 1, 1, 1, 1, 1; 1, 1, 1, 1, 1; 1, 1, 1, 1, 1; 1, 1, 1, 1, 1; 1, 1, 1, 1, 1];
 voxMat(:, :, 5) = [1, 1, 1, 1, 1; 1, 1, 1, 1, 1; 1, 1, 1, 1, 1; 1, 1, 1, 1, 1; 1, 1, 1, 1, 1; 1, 1, 1, 1, 1; 1, 1, 1, 1, 1];
 point = [0, 2.5, 2.5];
-rotation = 'deal(1, 0, 0)';
+[x, y, z] = deal(1, 0, 0);
 %}
 
 %neutron guide cube
@@ -196,7 +200,7 @@ voxMat(:, :, 3) = [1, 1, 7, 1, 1; 1, 1, 7, 1, 1; 7, 7, 7, 7, 7; 1, 1, 7, 1, 1; 1
 voxMat(:, :, 4) = [1, 1, 1, 1, 1; 1, 1, 1, 1, 1; 1, 1, 7, 1, 1; 1, 1, 1, 1, 1; 1, 1, 1, 1, 1];
 voxMat(:, :, 5) = [1, 1, 1, 1, 1; 1, 1, 1, 1, 1; 1, 1, 7, 1, 1; 1, 1, 1, 1, 1; 1, 1, 1, 1, 1];
 point = [2.5, 2.5, 2.5];
-rotation = 'OnUnitSphere';
+initRot = 1;
 %}
 
 %graphite building brick
@@ -204,13 +208,24 @@ rotation = 'OnUnitSphere';
 voxMat          = [7, 7, 7; 7, 3, 7; 7, 7, 7; 7, 3, 7; 7, 7, 7; 7, 3, 7; 7, 7, 7];
 voxMat(:, :, 2) = [7, 7, 7; 7, 3, 7; 7, 7, 7; 7, 3, 7; 7, 7, 7; 7, 3, 7; 7, 7, 7];
 point = [0, 1.5, 1];
-rotation = 'deal(1, 0, 0)';
+[x, y, z] = deal(1, 0, 0);
+%}
+
+%air with uranium voxel
+%
+voxMat          = [4, 4, 4, 4, 4; 4, 4, 4, 4, 4; 4, 4, 4, 4, 4; 4, 4, 4, 4, 4; 4, 4, 4, 4, 4];
+voxMat(:, :, 2) = [4, 4, 4, 4, 4; 4, 4, 4, 4, 4; 4, 4, 4, 4, 4; 4, 4, 4, 4, 4; 4, 4, 4, 4, 4];
+voxMat(:, :, 3) = [4, 4, 4, 4, 4; 4, 4, 4, 4, 4; 4, 4, 8, 4, 4; 4, 4, 4, 4, 4; 4, 4, 4, 4, 4];
+voxMat(:, :, 4) = [4, 4, 4, 4, 4; 4, 4, 4, 4, 4; 4, 4, 4, 4, 4; 4, 4, 4, 4, 4; 4, 4, 4, 4, 4];
+voxMat(:, :, 5) = [4, 4, 4, 4, 4; 4, 4, 4, 4, 4; 4, 4, 4, 4, 4; 4, 4, 4, 4, 4; 4, 4, 4, 4, 4];
+point = [0, 2.5, 2.5];
+[rx, ry, rz] = deal(1, 0, 0);
 %}
 
 %get the material info array
 for mat = 1:length(materials)
-    [t, a] = GetMat(materials{mat});
-    S(mat, :) = [t, a];
+    [t, a, ~, f] = GetMat(materials{mat});
+    S(mat, :) = [t, a, f];
 end
 
 %draw each voxel
@@ -228,7 +243,7 @@ if show
 end
 
 point = point*voxelSize;%scale the start point
-WoodcockVoxel(point, rotation, voxelSize, voxMat,S, neutrons, show);%calculate
+WoodcockVoxel(repmat(point, neutrons, 1), repmat(initRot, neutrons, 1), rx, ry, rz, voxelSize, voxMat,S, neutrons, show);%calculate
 
 if show
     axis equal tight;%make it show cubes so need same axis sizes
